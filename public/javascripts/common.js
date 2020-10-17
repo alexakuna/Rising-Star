9788db0443d9e51f421698056621513c1a93e4da
mac <mac@macs-MacBook-Air.local>
2020-10-17 20:04:24 +0300

    Chenged banner and add mobile menu

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,10 +1,14 @@
 
 // const win = window.querySelector('html')
 // const cords = win.getBoundingClientRect()
-const mainHeight = document.querySelector('.height-main')
-mainHeight.style.height = window.innerHeight - 64 + 'px'
+// const mainHeight = document.querySelector('.height-main')
+// mainHeight.style.height = window.innerHeight - 64 + 'px'
 // document.onmousemove = (e) => {
 //     console.log(e)
 // }
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+});
 
 // console.log(document.querySelectorAll('[data-act="active"]'))
