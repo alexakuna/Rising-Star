d1b8e1e17f7dc190496217314993d4fb7f5bf733
mac <mac@macs-MacBook-Air.local>
2020-10-18 12:41:47 +0300

    Search ip - test2

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,7 @@ const {Router} = require('express')
 const router = Router()
 
 router.get('/login', (req, res) => {
-    let ip = req.ip;
-    console.log(ip)
+    let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
     res.render('login', {title: 'Вход в админ-панель', ip: ip})
 })
