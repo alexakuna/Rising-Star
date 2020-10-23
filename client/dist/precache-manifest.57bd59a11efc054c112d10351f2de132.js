4cc78664bca37c2c69e7a58d05546af0e934c8db
mac <mac@macs-MacBook-Air.local>
2020-10-23 20:58:06 +0300

    test12

---
+++ client/dist/precache-manifest.57bd59a11efc054c112d10351f2de132.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326c5bbade37bc77e2d5",
+    "url": "/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "50a244842b7bf090f9db",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "c9b28c284cae66f7adefeefaa478b671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "326c5bbade37bc77e2d5",
+    "url": "/js/app.7f810156.js"
+  },
+  {
+    "revision": "cc6bd146d6ff892d3aa3",
+    "url": "/js/chunk-2d0b8e8f.361c10e2.js"
+  },
+  {
+    "revision": "7fc8de7f48068cf22f47",
+    "url": "/js/chunk-2d0c0fe6.681be953.js"
+  },
+  {
+    "revision": "b415852754a540449d18",
+    "url": "/js/chunk-2d0c7374.599954d6.js"
+  },
+  {
+    "revision": "5def1806fc3b5d1fdafb",
+    "url": "/js/chunk-2d0d3dce.a1683c15.js"
+  },
+  {
+    "revision": "5cac1003573696e7b47b",
+    "url": "/js/chunk-2d0d799a.f9f11569.js"
+  },
+  {
+    "revision": "40b615daa0e9a6e8779d",
+    "url": "/js/chunk-2d20f8d6.c9e7f704.js"
+  },
+  {
+    "revision": "1ab85267886ffbb7303f",
+    "url": "/js/chunk-2d21a3d2.9ebdbb55.js"
+  },
+  {
+    "revision": "6784e9b6dffb9d62ffa7",
+    "url": "/js/chunk-2d21b8a8.c5d9bc85.js"
+  },
+  {
+    "revision": "688aefbfc8c6bbc6fb1f",
+    "url": "/js/chunk-2d229481.0764a775.js"
+  },
+  {
+    "revision": "50a244842b7bf090f9db",
+    "url": "/js/chunk-vendors.a1914a01.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
