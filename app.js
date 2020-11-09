12a6e36e4feee8adf8af20e97c48c60904c6a266
mac <mac@macs-MacBook-Air.local>
2020-11-09 19:09:24 +0200

    fix pdf creating

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+
 const mongoose = require('mongoose')
-const path = require('path');
-//const loginPage = require('./routes/login')
-const homeRouter = require('./routes/home');
-const requestRoute = require('./routes/request');
-const aboutUsRoute = require('./routes/aboutus');
-const contactsRoute = require('./routes/contacts');
-const feedbacksRoute = require('./routes/feedbacks');
-const regulationsRoute = require('./routes/regulations');
-const timetableRoute = require('./routes/timetable');
-const videoRoute = require('./routes/video');
+const path = require('path')
+const pdf = require('express-pdf')
+const axios = require('axios')
+const cors = require('cors')
+
+const homeRouter = require('./routes/home')
+const requestRoute = require('./routes/request')
+const aboutUsRoute = require('./routes/aboutus')
+const contactsRoute = require('./routes/contacts')
+const feedbacksRoute = require('./routes/feedbacks')
+const regulationsRoute = require('./routes/regulations')
+const timetableRoute = require('./routes/timetable')
+const videoRoute = require('./routes/video')
 const regulationsVocal = require('./routes/regulations/vocal')
 const regulationsInstrumental = require('./routes/regulations/instrumental')
 const regulationsArt = require('./routes/regulations/art')
 const regulationsCircus = require('./routes/regulations/circus')
-const regulationsTeatr = require('./routes/regulations/teatr')
+const regulationsTheatre = require('./routes/regulations/teatr')
 const regulationsDance = require('./routes/regulations/dance')
-const app = express();
-const pdf = require('express-pdf')
-const axios = require('axios')
-const cors = require('cors')
-app.use(pdf)
-//const jsdom = require("jsdom")
-//const { JSDOM } = jsdom;
-
 
 if (typeof localStorage === "undefined" || localStorage === null) {
     let LocalStorage = require('node-localstorage').LocalStorage;
     localStorage = new LocalStorage('./scratch');
 }
-
 
 const uri = 'mongodb+srv://admin:15021979@rises.brol3.mongodb.net/home_page'
 
@@ -38,13 +34,14 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     .catch(err => console.log(err))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
+app.use(pdf)
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('*', async (req, res, next) => {
     app.locals.activeUrl = req.params[0]
@@ -61,49 +58,50 @@ app.get('*', async (req, res, next) => {
     next()
 })
 
-app.use(homeRouter);
-app.use(requestRoute);
-app.use(contactsRoute);
-app.use(feedbacksRoute);
-app.use(regulationsRoute);
-app.use(aboutUsRoute);
-app.use(timetableRoute);
-app.use(videoRoute);
-
+// Роутеры навигации
+app.use(homeRouter)
+app.use(requestRoute)
+app.use(contactsRoute)
+app.use(feedbacksRoute)
+app.use(regulationsRoute)
+app.use(aboutUsRoute)
+app.use(timetableRoute)
+app.use(videoRoute)
 
 // Роутеры для положений
-app.use(regulationsVocal);
-app.use(regulationsArt);
-app.use(regulationsInstrumental);
-app.use(regulationsCircus);
-app.use(regulationsTeatr);
-app.use(regulationsDance);
-
-
-
-app.use('/pdfFromHTML', function(req, res){
-    res.pdfFromHTML({
-        filename: 'generated.pdf',
-        html: path.resolve(__dirname, './template.html'),
-        // options: {...}
-    });
-});
+app.use(regulationsVocal)
+app.use(regulationsArt)
+app.use(regulationsInstrumental)
+app.use(regulationsCircus)
+app.use(regulationsTheatre)
+app.use(regulationsDance)
 
 app.use('/pdfFromHTMLString', function(req, res){
-    axios.get('https://rsfrontend.herokuapp.com' + localStorage.getItem('url'))
+    // Перед продакшеном обязатаельно поменять локальный url на url домена где будет сайт
+    const url = localStorage.getItem('url')
+    axios.get(`http://localhost:4200${url}`)
         .then(resp => {
-        res.pdfFromHTML({
-            filename: 'generated.pdf',
-            htmlContent: resp.data,
-            options: {"base": "https://rsfrontend.herokuapp.com/stylesheets/materialize.min.css"}
-        });
-    })
-});
 
-app.use('/pdf', function(req, res){
-    res.pdf(path.resolve(__dirname, 'regulation.pdf'));
+            const str = resp.data.indexOf('main')
+            const str2 = resp.data.lastIndexOf('main')
+
+            const result = resp.data.substring(str - 1, str2 + 5)
+            console.log(result)
+        res.pdfFromHTML({
+            filename: `${url.slice(0, 0)}.pdf`,
+            htmlContent: result,
+            // Перед продакшеном обязатаельно поменять локальный url на url домена где будет сайт
+            options: {
+                "base": "http://localhost:4200/stylesheets/materialize.min.css",
+                "border": {
+                    "top": "1in",
+                    "right": "1in",
+                    "bottom": "1in",
+                    "left": "1in"
+                }
+            }
+        })
+    }).catch(e => console.warn(e))
 })
 
-
-
-module.exports = app;
+module.exports = app
