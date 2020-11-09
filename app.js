e9fe03f416da6be9dfd751a16c7aca4dcb7bd470
mac <mac@macs-MacBook-Air.local>
2020-11-09 12:19:27 +0200

    backup

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ const regulationsCircus = require('./routes/regulations/circus')
 const regulationsTeatr = require('./routes/regulations/teatr')
 const regulationsDance = require('./routes/regulations/dance')
 const app = express();
+const pdf = require('express-pdf')
+const axios = require('axios')
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom;
+
 
 const uri = 'mongodb+srv://admin:15021979@rises.brol3.mongodb.net/home_page'
 
@@ -31,8 +36,6 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.locals.isActive = true
 
 app.get('*', async (req, res, next) => {
     app.locals.activeUrl = req.params[0]
@@ -67,5 +70,43 @@ app.use(regulationsInstrumental);
 app.use(regulationsCircus);
 app.use(regulationsTeatr);
 app.use(regulationsDance);
+
+// const file = require('./exemple')
+// app.post('/create-pdf', (req, res) => {
+//
+//     pdf.create('', {}).toFile('regulation.pdf', (err) => {
+//         if(err) {
+//             res.send(Promise.reject());
+//         }
+//         res.send(Promise.resolve());
+//     })
+// })
+app.use(pdf)
+
+app.use('/pdfFromHTML', function(req, res){
+    res.pdfFromHTML({
+        filename: 'generated.pdf',
+        html: path.resolve(__dirname, './template.html'),
+        // options: {...}
+    });
+});
+
+app.use('/pdfFromHTMLString', function(req, res){
+    axios.get('http://localhost:4200/vocal').then(resp => {
+        //const dom = new JSDOM(resp.data);
+        //console.log(dom.window.document.querySelector("p").textContent)
+        res.pdfFromHTML({
+            filename: 'generated.pdf',
+            htmlContent: resp.data,
+            // options: {...}
+        });
+    })
+});
+
+app.use('/pdf', function(req, res){
+    res.pdf(path.resolve(__dirname, 'regulation.pdf'));
+})
+
+
 
 module.exports = app;
--- routes/regulations/vocal.js
+++ routes/regulations/vocal.js
@@ -2,8 +2,7 @@ const {Router} = require('express')
 const router = Router()
 
 router.get('/vocal', (req, res) => {
-
-    res.render('regulations/vocal', {title: 'Вокал'})
+    res.render('regulations/vocal', {title: 'Вокал', id: '/vocal'})
 })
 
 module.exports = router;
