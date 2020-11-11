5a5b4aa328e52e114a1a0d02fad35d7e762e0bd3
mac <mac@macs-MacBook-Air.local>
2020-11-11 13:31:19 +0200

    add model for vocal page, set output data from bd in vocal.ejs page

--- routes/regulations/vocal.js
+++ routes/regulations/vocal.js
@@ -1,8 +1,12 @@
 const {Router} = require('express')
 const router = Router()
+const Vocal = require('../../models/Regulation')
 
-router.get('/vocal', (req, res) => {
-    res.render('regulations/vocal', {title: 'Вокал', id: '/vocal'})
+router.get('/vocal', async(req, res) => {
+
+    const content = await Vocal.find()
+
+    res.render('regulations/vocal', content[0])
 })
 
 module.exports = router;
