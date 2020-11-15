ff04f98db288cf20b1f14454c283810cf60a5082
mac <mac@macs-MacBook-Air.local>
2020-11-15 18:17:25 +0200

    123123123

--- middleware/upload.js
+++ middleware/upload.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
     if (
-        file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'video/quicktime') {
+        file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'video/quicktime' || file.mimetype === 'video/mp4') {
         cb(null, true)
     } else {
         cb(null, false)
