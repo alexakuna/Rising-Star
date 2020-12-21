1e6fd9347393c47c021cb8e6f8941505f0b343aa
mac <alexxjc@gmail.com>
2020-12-21 02:33:36 +0200

    setting reulations page

--- models/Regulations.js
+++ models/Regulations.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
-    regulations: [{img: String, title: String, url: String}]
+    regulations: [{img: String, title: String, url: String, show: Boolean}]
 })
 
 module.exports = model('Regulations', schema)
