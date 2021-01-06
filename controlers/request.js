6cfee308b7a627419ca3f93ec97c479810a1effc
mac <alexxjc@gmail.com>
2021-01-06 23:52:02 +0200

    work with home page

--- controlers/request.js
+++ controlers/request.js
@@ -92,7 +92,7 @@ module.exports.request = async function (req, res) {
         console.log(e)
     }
 
-    function helpPushVideo() { //
+    function helpPushVideo() {
         if(newPathVideo) {
             images.push({
                 filename: newNameVideo,
@@ -104,53 +104,3 @@ module.exports.request = async function (req, res) {
         }
     }
 }
-
-// module.exports.getById = async function (req, res) {
-//     try {
-//         const category = await FormRequest.findById(req.params.id);
-//         res.status(200).json(category);
-//     } catch (e) {
-//        console.log(res, e);
-//     }
-// };
-//
-// module.exports.remove = async function (req, res) {
-//     try {
-//         await FormRequest.remove({_id: req.params.id});
-//         await FormRequest.remove({category: req.params.id});
-//         res.status(200).json({message: 'Category has been delete.'});
-//     } catch (e) {
-//         console.log(res, e);
-//     }
-// };
-//
-// module.exports.create = async function (req, res) {
-//     const category = new FormRequest({
-//         name: req.body.name,
-//         user: req.user.id,
-//         imageSrc: req.file ? req.file.path : ''
-//     });
-//     try {
-//         await category.save();
-//         res.status(201).json(category)
-//     } catch (e) {
-//         console.log(res, e);
-//     }
-// };
-//
-// module.exports.update = async function (req, res) {
-//     const updated = {name: req.body.name};
-//     if (req.file) {
-//         updated.imageSrc = req.file.path
-//     }
-//     try {
-//         const category = await FormRequest.findOneAndUpdate(
-//             {_id: req.params.id},
-//             {$set: updated},
-//             {new: true}
-//         )
-//         res.status(200).json(category)
-//     } catch (e) {
-//         console.log(res, e);
-//     }
-// };
--- models/Home.js
+++ models/Home.js
@@ -1,10 +1,23 @@
+
 const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
     title: String,
     description: String,
+    subtitle: String,
     banner: String,
-    news: Array
-})
+    news: Array,
+    sections: [
+        {
+            title: String,
+            images: Array,
+            description: Array,
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+}, {collection: 'homes'})
 
 module.exports = model('Home', schema)
